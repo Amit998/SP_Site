bda3110f60001fce966e2eb4986e5bbf4706d4e2
Amit998 <damitlucky998@gmail.com>
2020-09-01 10:41:17 +0530

    modified

--- for-recruiters.php
+++ for-recruiters.php
@@ -5,11 +5,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Commers</title>
+    <title>SAFEDOCS TECHNOLOGY</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="/style3.css">
-    <link rel="stylesheet" href="for-recruiters.css">
+
+  
     
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
@@ -17,14 +17,14 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
  
-    
+    <link rel="stylesheet" href="./style.css" />
+    <link rel="stylesheet" href="for-recruiters.css">
 </head>
 <body>
 
   <!-- NAVBAR -->
-
   <nav class="navbar navbar-expand-lg fixed-top py-3">
-    <a href="./index3.html" class="navbar-brand">AMT</a>
+    <a href="./index.html" class="navbar-brand">SAFEDOCS TECHNOLOGY</a>
     <button
       class="navbar-toggler custom-toggler"
       type="button"
@@ -36,25 +36,15 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mx-auto">
         <li class="nav-item active">
-          <a href="./index3.html" class="nav-link">Home</a>
+          <a href="./index.html" class="nav-link">Home</a>
         </li>
+ 
         <li class="nav-item">
-          <a href="./services" class="nav-link">Services</a>
+          <a href="./shop.html" class="nav-link">Products</a>
         </li>
 
         <li class="nav-item">
-          <a href="./shop.html" class="nav-link">Shop</a>
-        </li>
-
-
-        <li class="nav-item">
-          <a href="./blog.html" class="nav-link">Blog</a>
-        </li>
-        <li class="nav-item">
-          <a href="#contact" class="nav-link">Contact </a>
-        </li>
-        <li class="nav-item login-lin">
-          <a href="./form.html" class="nav-link k">Login <i class="fa fa-user"></i></a>
+          <a  href="tel:+918210431630" class="nav-link">Contact : +918210431630 </a>
         </li>
       </ul>
 
@@ -68,34 +58,37 @@
       </ul>
     </div>
   </nav>
+  <br>
+
 
   <!-- NAV BAR END -->
 
   <!-- ShoCase For HR -->
 
   <div class="fr-showCase">
-    <h1>Well Come To Our Services</h1>
+    <h1>Welcome To the JOB World</h1>
     <p>100’s of employees placed in jobs every month!</p>
+    <p>Looking for a Job</p>
     <div class="fr-btn">
-      <a href="" class="ft-btn1">Loaking For A Job?</a>
-      <a href="" class="ft-btn2">Looking To Hire</a>
+      <a href="tel:5554280940" class="ft-btn1">Call Now</a>
     </div>
   </div>
 
-  <section id="section-a">
-    <h2>For Job Seeker</h2>
-    <p>Hi there. You may select what trade do you belong to & upload your resume after filling the necessary information in the form. We got your back. So once we will get the client interested in your profile we will make sure about the your interview dates. </p>
+  <section id="section-a" class="container">
+    <h2 style="color: #40514e;">For Job Seeker</h2>
+    <p style="color: #40514e;">Hi there. You may select what trade do you belong to & upload your resume after filling the necessary information in the form. We got your back. So once we will get the client interested in your profile we will make sure about the your interview dates. </p>
   </section>
-  <section id="section-b">
-    <h2>For Hiring Manpower</h2>
-    <p>Greetings. Welcome to Safethos Technologies where we are known to take over Staffing challenges of Organizations looking ahead to grow in the terms of Market Capitalization & Expertise in respective areas by letting us hire for you. Just post your query on specific trade candidate(s) you are looking for. Or drop a Job Description so that we can do the First level Screening for you.</p>
+  <hr>
+  <section id="section-b" style="color: #40514e; " class="container">
+    <h2 style="color: #40514e;">For Hiring Manpower</h2>
+    <p style="color: #40514e;">Greetings. Welcome to Safethos Technologies where we are known to take over Staffing challenges of Organizations looking ahead to grow in the terms of Market Capitalization & Expertise in respective areas by letting us hire for you. Just post your query on specific trade candidate(s) you are looking for. Or drop a Job Description so that we can do the First level Screening for you.</p>
   </section>
   <section id="section-c">
     
     <div class="box-1">
 
       <h3>Industries We Serve</h3>
-      <ol>
+      <ol style="color: #40514e;">
         <li>Tata Steel Contractors</li>
         <li>Call Center</li>
         <li>Sales</li>
@@ -143,22 +136,13 @@
     <div class="footer">
       <div class="inner-footer">
         <div class="footer-items">
-          <h1>Computerts & Codes</h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ut accusantium et veritatis suscipit,
-            consequuntur omnis ab ratione debitis? Quasi voluptate laborum dolorum, quo voluptates quis beatae. Maiores,
-            dolorum provident!
-          </p>
-        </div>
-
-        <div class="footer-items">
           <h2>Our Works</h2>
           <div class="footer-border"></div>
           <ul>
-            <a href=""><li>Work 1</li></a>
-            <a href=""><li>Work 2</li></a>
-            <a href=""><li>Work 3</li></a>
-            <a href=""><li>Work 4</li></a>
+            <a href="#"><li>Work 1</li></a>
+            <a href="#"><li>Work 2</li></a>
+            <a href="#"><li>Work 3</li></a>
+            <a href="#"><li>Work 4</li></a>
           </ul>
         </div>
 
@@ -176,14 +160,19 @@
           <h2>Contact</h2>
           <div class="footer-border"></div>
           <ul>
-            <li><i class="fa fa-map-marker" aria-hidden="true"></i> xyx street</li>
             <li>
-              <i class="fa fa-phone" aria-hidden="true"></i>
-              9798798678
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+           
+                Jamshedpur,West Bengal, India
+ 
+            </li>
+            <li>
+        <i class="fa fa-phone" aria-hidden="true"></i><a href="tel:+918210431630">+918210431630</a>
+              
             </li>
             <li>
               <i class="fa fa-envelope" aria-hidden="true"></i>
-              vjhcc@gmail.com
+              <a href="mailto:safedocstechnology@gmail.com">safedocstechnology@gmail.com</a>
             </li>
           </ul>
 
@@ -195,7 +184,9 @@
           </div>
         </div>
       </div>
-      <div class="footer-bottom">Copyright &copy; Computer & Codes 2020. All Rights Reserved</div>
+      <div class="footer-bottom">
+        Copyright &copy; Computer & Codes 2020. All Rights Reserved
+      </div>
     </div>
 
     <a href="#" class="gotoTop"><i class="fa fa-arrow-up"></i></a>
--- shop.php
+++ shop.php
@@ -5,10 +5,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Commers</title>
+    <title>SAFEDOCS TECHNOLOGY</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="productList.css">
-    <link rel="stylesheet" href="style3.css">
+    
     
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
@@ -17,12 +16,15 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.js" integrity="sha256-kiFgik3ybDpn1VOoXqQiaSNcpp0v9HQZFIhTgw1c6i0=" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="productList.css">
+    
 </head>
 <body>
   <!-- NAVBAR -->
 
   <nav class="navbar navbar-expand-lg fixed-top py-3">
-    <a href="./index3.html" class="navbar-brand">AMT</a>
+    <a href="./index.html" class="navbar-brand">SAFEDOCS TECHNOLOGY</a>
     <button
       class="navbar-toggler custom-toggler"
       type="button"
@@ -34,25 +36,15 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mx-auto">
         <li class="nav-item active">
-          <a href="./index3.html" class="nav-link">Home</a>
+          <a href="./index.html" class="nav-link">Home</a>
         </li>
+ 
         <li class="nav-item">
-          <a href="./services" class="nav-link">Services</a>
+          <a href="./shop.html" class="nav-link">Products</a>
         </li>
 
         <li class="nav-item">
-          <a href="./shop.html" class="nav-link">Shop</a>
-        </li>
-
-
-        <li class="nav-item">
-          <a href="./blog.html" class="nav-link">Blog</a>
-        </li>
-        <li class="nav-item">
-          <a href="#contact" class="nav-link">Contact </a>
-        </li>
-        <li class="nav-item login-lin">
-          <a href="./form.html" class="nav-link k">Login <i class="fa fa-user"></i></a>
+          <a  href="tel:+918210431630" class="nav-link">Contact : +918210431630 </a>
         </li>
       </ul>
 
@@ -67,7 +59,6 @@
     </div>
   </nav>
   <br>
-
   <!-- NAV BAR END -->
 
     <section class="shop-section">
@@ -83,7 +74,7 @@
                       <div class="col-md-4 product-grid" > 
                         <div class="image">
                             <a href="">
-                                <img src="./img/1.jpg" alt="" class="w-100">
+                                <img src="./img/9.jpg" alt="" class="w-100">
                                 
                                 <div class="overlay">
                                     <div class="detail"><span class="">VIEW DETAILS</span></div>
@@ -92,9 +83,9 @@
                         </div>
                         
                           <h4 class="text-center">Name Of The Product</h4>
-                          <h5 class="text-center">Price</b></h5>
+                          <h5 class="text-center">Price</h5>
                           <h6 class="text-center">Amount Available</b></h6>
-                          <h6 class="text-center">Raiting:</b></h6>
+                          <h6 class="text-center">Raiting:</h6>
                                   
                         <!--a href="" type="submit" class="btn cartBtn" id="addToCart" >ADD TO CART</a-->
                         <div class="form-group">
@@ -112,9 +103,40 @@
 
 
                         <div class="col-md-4 product-grid" > 
+                          <div class="image">
+                              <a href="">
+                                  <img src="./img/14.jpg" alt="" class="w-100">
+                                  
+                                  <div class="overlay">
+                                      <div class="detail"><span class="">VIEW DETAILS</span></div>
+                                  </div>
+                              </a>
+                          </div>
+                          
+                            <h4 class="text-center">Name Of The Product</h4>
+                            <h5 class="text-center">Price</h5>
+                            <h6 class="text-center">Amount Available</b></h6>
+                            <h6 class="text-center">Raiting:</h6>
+                                    
+                          <!--a href="" type="submit" class="btn cartBtn" id="addToCart" >ADD TO CART</a-->
+                          <div class="form-group">
+                            <div class="form-check">
+                              <input type="checkbox" class="form-check-input" name="object1">
+                              <label class="form-check-label" for="exampleCheck1">Select This If You Want To add it into your Wishlish</label>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <input class="form-control" id="exampleInputEmail1" aria-describedby="totalAmount" type="number" name="quantity1" placeholder="quantity" min="0" max="10">
+                            <small id="totalAmount" class="form-text text-muted">Enter The Amount.</small>
+                          </div>
+  
+                          </div>
+                        
+
+                        <div class="col-md-4 product-grid" > 
                         <div class="image">
                             <a href="">
-                                <img src="./img/1.jpg" alt="" class="w-100">
+                                <img src="./img/10.jpg" alt="" class="w-100">
                                 
                                 <div class="overlay">
                                     <div class="detail"><span class="">VIEW DETAILS</span></div>
@@ -145,7 +167,7 @@
                         <div class="col-md-4 product-grid" > 
                         <div class="image">
                             <a href="">
-                                <img src="./img/1.jpg" alt="" class="w-100">
+                                <img src="./img/11.jpg" alt="" class="w-100">
                                 
                                 <div class="overlay">
                                     <div class="detail"><span class="">VIEW DETAILS</span></div>
@@ -178,7 +200,7 @@
                         <div class="col-md-4 product-grid" > 
                         <div class="image">
                             <a href="">
-                                <img src="./img/1.jpg" alt="" class="w-100">
+                                <img src="./img/12.jpg" alt="" class="w-100">
                                 
                                 <div class="overlay">
                                     <div class="detail"><span class="">VIEW DETAILS</span></div>
@@ -210,7 +232,7 @@
                         <div class="col-md-4 product-grid" > 
                         <div class="image">
                             <a href="">
-                                <img src="./img/1.jpg" alt="" class="w-100">
+                                <img src="./img/13.jpg" alt="" class="w-100">
                                 
                                 <div class="overlay">
                                     <div class="detail"><span class="">VIEW DETAILS</span></div>
@@ -272,6 +294,7 @@
     
 
             <input type="submit" name="btn" class="btn btn-primary">
+            <input type="hidden" name="form_submitted" value="1">
 
 
 
@@ -283,14 +306,13 @@
              
              
 <?php
-if(isset($_POST['btn'])){
+if(isset($_POST['btn']) and isset($_POST["form_submitted"])){
   $email= $_POST["inputEmail"];
   $phn= $_POST["phn"];
   $name=$_POST["inputName"];
   $text=$_POST["text"];
  
 
-  
 ?>
 
 
@@ -333,19 +355,19 @@ if(isset($_POST['btn'])){
                         $object5=0;
                     }
                     
-                    $to="pollabrock@gmail.com";
+                    $to="damitlucky998@gmail.com";
                     $subject="Asking for Quotation";
                     
-                    $message="Mail from:".$email."\n"."Name:".$name."\n"."Phn number=".$phn."\n"."Object1 quantity=".$object1."\n"."Object2 quantity=".$object2."\n"."Object3 quantity=".$object3."\n"."Object4 quantity=".$object4."\n"."Message :".$text;                    
+                    $message="Mail from:".$email."\n"."Name:".$name."\n"."Phn number=".$phn."\n"."Object1 quantity=".$object1."\n"."Object2 quantity=".$object2."\n"."Object3 quantity=".$object3."\n"."Object4 quantity=".$object4."\n  object 5".$object5."\n"."Message :".$text;                    
                     
                     if(mail($to,$subject,$message)){
                         ?>
-            <script>alert("successful");</script>
+            <script>console.log("successful");</script>
                     <?php
                       
                     } else{
                         ?>
-            <script>alert("unsuccessful");</script>
+            <script>console.log("unsuccessful");</script>
                         <?php  
                             }
                     }
@@ -364,22 +386,13 @@ if(isset($_POST['btn'])){
     <div class="footer">
       <div class="inner-footer">
         <div class="footer-items">
-          <h1>Computerts & Codes</h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ut accusantium et veritatis suscipit,
-            consequuntur omnis ab ratione debitis? Quasi voluptate laborum dolorum, quo voluptates quis beatae. Maiores,
-            dolorum provident!
-          </p>
-        </div>
-
-        <div class="footer-items">
           <h2>Our Works</h2>
           <div class="footer-border"></div>
           <ul>
-            <a href=""><li>Work 1</li></a>
-            <a href=""><li>Work 2</li></a>
-            <a href=""><li>Work 3</li></a>
-            <a href=""><li>Work 4</li></a>
+            <a href="#"><li>Work 1</li></a>
+            <a href="#"><li>Work 2</li></a>
+            <a href="#"><li>Work 3</li></a>
+            <a href="#"><li>Work 4</li></a>
           </ul>
         </div>
 
@@ -397,14 +410,19 @@ if(isset($_POST['btn'])){
           <h2>Contact</h2>
           <div class="footer-border"></div>
           <ul>
-            <li><i class="fa fa-map-marker" aria-hidden="true"></i> xyx street</li>
             <li>
-              <i class="fa fa-phone" aria-hidden="true"></i>
-              9798798678
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+           
+                Jamshedpur,West Bengal, India
+ 
+            </li>
+            <li>
+        <i class="fa fa-phone" aria-hidden="true"></i><a href="tel:+918210431630">+918210431630</a>
+              
             </li>
             <li>
               <i class="fa fa-envelope" aria-hidden="true"></i>
-              vjhcc@gmail.com
+              <a href="mailto:safedocstechnology@gmail.com">safedocstechnology@gmail.com</a>
             </li>
           </ul>
 
@@ -416,7 +434,9 @@ if(isset($_POST['btn'])){
           </div>
         </div>
       </div>
-      <div class="footer-bottom">Copyright &copy; Computer & Codes 2020. All Rights Reserved</div>
+      <div class="footer-bottom">
+        Copyright &copy; Computer & Codes 2020. All Rights Reserved
+      </div>
     </div>
 
     
